b4a7e9890b83a010ca45480076a43ca180496143
Hilario <hilariofjunior@live.com>
2026-05-06 19:59:48 -0300

    new theorems

--- NaturalNumberGame/NaturalNumberGame.lean
+++ NaturalNumberGame/NaturalNumberGame.lean
@@ -54,6 +54,7 @@ theorem prop_1_2_alt3 : 2 = Nat.succ (Nat.succ 0) := by
 theorem prop_1_3 (a b c : ℕ) : a + (b+0) + (c+0) = a + b + c := by
   rw [Nat.add_zero] -- rewrite b+0 as b; goal becomes a + b + (c+0) = a + b + c
   rw [Nat.add_zero] -- rewrite c+0 as c; goal becomes a + b + c = a + b + c
+  -- closed by rfl automatically
 
 -- Proposition 1.4
 
@@ -61,9 +62,9 @@ theorem prop_1_4 (n : ℕ) : Nat.succ n = n + 1 := by
   rfl -- closed by rfl automatically since `n + 1` and `Nat.succ n` are definitionally equal
 
 theorem prop_1_4alt (n : ℕ) : Nat.succ n = n + 1 := by
-  rewrite [one_eq_succ_zero] -- rewrite 1 as succ 0; goal becomes n.succ = n + succ 0
-  rewrite [Nat.add_succ] -- rewrite n + succ 0 as (n + 0).succ; goal becomes n.succ = (n + 0).succ
-  rewrite [Nat.add_zero] -- rewrite n + 0 as n; goal becomes n.succ = n.succ
+  rewrite [one_eq_succ_zero] -- rewrite 1 as succ 0; goal becomes succ n = n + succ 0
+  rewrite [Nat.add_succ] -- rewrite n + succ 0 as succ (n + 0); goal becomes succ n = succ (n + 0)
+  rewrite [Nat.add_zero] -- rewrite n + 0 as n; goal becomes succ n = succ n
   rfl -- closed by rfl
 
 -- Proposition 1.5
@@ -80,15 +81,17 @@ theorem prop_1_5_alt : 2 + 2 = 4 := by
   rewrite [three_eq_succ_two] -- rewrite 3 as succ 2; goal becomes 2 + 2 = succ succ 2
   nth_rewrite 2 [two_eq_succ_one] -- rewrite the second 2 as succ 1;
                                   -- goal becomes 2 + succ 1 = succ succ 2
-  rewrite[Nat.add_succ] -- rewrite 2 + succ 1 as (2 + 1).succ;
-                         -- goal becomes (2 + 1).succ = succ succ 2
+  rewrite[Nat.add_succ] -- rewrite 2 + succ 1 as succ (2 + 1);
+                         -- goal becomes succ (2 + 1) = succ (succ 2)
   nth_rewrite 1 [← Nat.succ_eq_add_one] -- rewrite 2 + 1 as succ 2;
-                                     -- goal becomes (succ 2).succ = succ succ 2
+                                     -- goal becomes succ (succ 2) = succ (succ 2)
   rfl -- closed by rfl
 
 -- -------------------------------------------------------
 -- Addition World
 -- -------------------------------------------------------
+
+-- Proposition 2.1
 
 theorem prop_2_1 (n : ℕ) : 0 + n = n := by
   induction n with -- This is the induction statement.
@@ -101,17 +104,44 @@ theorem prop_2_1 (n : ℕ) : 0 + n = n := by
       rw [ih] -- rewrite 0 + d as d; goal becomes succ d = succ d
       -- closed by rfl automatically
 
-theorem prop_2_1alt (n : ℕ) : 0 + n = n := by
-  induction n with  -- This is the induction statement.
-    | zero => -- This is the base case. The goal is 0 + 0 = 0.
-      rewrite [Nat.add_zero] -- rewrite 0 + 0 as 0; goal becomes 0 = 0
-      rfl -- closed by rfl
-    | succ d ih => -- This is the induction step.
-                   -- The goal is 0 + succ d = succ d given that 0 + d = d (ih).
-      rewrite [Nat.add_succ] -- rewrite 0 + succ d as succ (0 + d);
-                             -- goal becomes succ (0 + d) = succ d
-      rewrite [ih] -- rewrite 0 + d as d; goal becomes succ d = succ d
-      rfl -- closed by rfl
+-- Proposition 2.2
+
+theorem prop_2_2 (a b : ℕ) : Nat.succ a + b = Nat.succ (a + b) := by
+  induction b with -- This is the induction statement.
+  | zero => -- This is the base case. The goal is succ a + 0 = succ (a + 0).
+    rw [Nat.add_zero] -- rewrite succ a + 0 as succ a and a + 0 as a;
+                      -- goal becomes succ a = succ a
+    -- closed by rfl automatically
+  | succ d ih => -- This is the induction step.
+                 -- The goal is succ a + succ d = succ (a + succ d) given that
+                 --succ a + d = succ (a + d) (ih).
+    rw [Nat.add_succ] -- rewrite succ a + succ d as succ (succ a + d);
+                      -- goal becomes succ (succ a + d) = succ (a + succ d)
+    rw [Nat.add_succ] -- rewrite a + succ d as succ (a + d);
+                      -- goal becomes succ (succ a + d) = succ (succ (a + d))
+    rw [ih] -- rewrite succ a + d as succ (a + d);
+           -- goal becomes succ (succ (a + d)) = succ (succ (a + d))
+    -- closed by rfl automatically
+
+theorem prop_2_2_alt (a b : ℕ) : Nat.succ a + b = Nat.succ (a + b) := by
+  induction b with -- This is the induction statement.
+  | zero => -- This is the base case. The goal is succ a + 0 = succ (a + 0).
+    rewrite [Nat.add_zero] -- rewrite succ a + 0 as succ a;
+                           -- goal becomes succ a = succ (a + 0)
+    rewrite [Nat.add_zero] -- rewrite a + 0 as a;
+                           -- goal becomes succ a = succ a
+    rfl -- closed by rfl automatically
+  | succ d ih => -- This is the induction step.
+                 -- The goal is succ a + succ d = succ (a + succ d) given that
+                 -- succ a + d = succ (a + d) (ih).
+    rewrite [Nat.add_succ] -- rewrite succ a + succ d as succ (succ a + d);
+                           -- goal becomes succ (succ a + d) = succ (a + succ d)
+    rewrite [Nat.add_succ] -- rewrite a + succ d as succ (a + d);
+                           -- goal becomes succ (succ a + d) = succ (succ (a + d))
+    rewrite [ih] -- rewrite succ a + d as succ (a + d);
+                 -- goal becomes succ (succ (a + d)) = succ (succ (a + d))
+    rfl -- closed by rfl
+
 
 
 
